6593df682f27fac55de83419d5e6c755b6a048dc
Sergi Granell <xerpi.g.12@gmail.com>
2014-10-12 18:58:45 +0200

    Added new files

---
+++ include/utils.h
@@ -0,0 +1,25 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#define VID 0x054C
+#define PID 0x05C4
+
+#include <gccore.h>
+#include <bte/bte.h>
+
+extern int run;
+
+void init_stuff();
+void init_video();
+void print_bd_addr(struct bd_addr *bdaddr);
+void print_mac(uint8_t *mac);
+int bte_read_bdaddr_cb(s32 result, void *userdata);
+struct bd_addr bdaddr;
+
+void find_and_set_mac();
+int get_bdaddrs(int fd, unsigned char *paired_mac, unsigned char *ds4_mac);
+int set_paired_mac(int fd, unsigned char *mac);
+int get_link_key(int fd, unsigned char *link_key);
+
+
+#endif
---
+++ source/utils.c
@@ -0,0 +1,216 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fat.h>
+#include <wiiuse/wpad.h>
+#include "utils.h"
+#include "ds4wiibt_config.h"
+
+int run = 1;
+static int bd_addr_read = 0;
+static int heap_id = -1;
+static void *xfb = NULL;
+static GXRModeObj *rmode = NULL;
+
+static void button_cb()
+{
+	run = 0;
+}
+
+void init_stuff()
+{
+	SYS_SetResetCallback(button_cb);
+	SYS_SetPowerCallback(button_cb);
+	fatInitDefault();
+	init_video();
+	WPAD_Init();
+	heap_id = iosCreateHeap(4096);
+}
+
+void find_and_set_mac()
+{
+	bd_addr_read = 0;
+	BTE_ReadBdAddr(&bdaddr, bte_read_bdaddr_cb);
+	
+	//Wait until we get the callback...
+	while (!bd_addr_read) usleep(1000);
+		
+	//Find the DS4 controller
+	usb_device_entry dev_entry[8];
+	unsigned char dev_count;
+	if (USB_GetDeviceList(dev_entry, 8, USB_CLASS_HID, &dev_count) < 0) {
+		printf("Error getting USB device list.\n"); return;
+	}
+		
+	int i;
+	usb_device_entry *de;
+	for (i = 0; i < dev_count; ++i) {
+		de = &dev_entry[i];
+		
+		if( de->vid == VID && de->pid == PID) {
+			printf("DS4 found.\n");
+			
+			int fd;
+			if (USB_OpenDevice(de->device_id, VID, PID, &fd) < 0) {
+				printf("Could not open the DS4.\n"); return;
+			}
+		
+			printf("Wii local BD MAC: ");
+			print_mac(bdaddr.addr);
+			printf("\n");
+			
+			unsigned char paired[6] = {0}, ds4_mac[6] = {0};
+			get_bdaddrs(fd, paired, ds4_mac);
+
+			//Write MAC to config
+			struct ds4wiibt_config_ctx conf;
+			ds4wiibt_config_initialize(&conf);
+			if (ds4wiibt_config_read(DS4WIIBT_CONFIG_FILE, &conf)) {
+				printf("Opened config file\n");
+				if (!ds4wiibt_config_MAC_exists(&conf, ds4_mac)) {
+					printf("MAC doesnt exist, adding it\n");
+					ds4wiibt_config_add(&conf, ds4_mac);
+					ds4wiibt_config_write(DS4WIIBT_CONFIG_FILE, &conf);
+				} else {
+					printf("MAC already exists\n");
+				}
+				ds4wiibt_config_free(&conf);
+			} else {
+				printf("Creating config file... ");
+				ds4wiibt_config_add(&conf, ds4_mac);
+				if (ds4wiibt_config_write(DS4WIIBT_CONFIG_FILE, &conf) > 0) {
+					printf("Done!\n");
+					ds4wiibt_config_free(&conf);
+				} else {
+					printf("Error.\n");
+				}
+			}
+			
+			printf("Controller's bluetooth MAC address: ");
+			print_mac(ds4_mac);
+			printf("\n");
+			printf("\nCurrent controller paired address: ");
+			print_mac(paired);
+
+			struct bd_addr bd2;
+			memcpy(bd2.addr, paired, sizeof(uint8_t) * 6);
+			if (bd_addr_cmp(&bdaddr, &bd2)) {
+				printf("\n\nAddress is already set! Press HOME to exit.\n");
+				return;
+			}		
+			
+			printf("\nSetting the pair address...");
+			uint8_t *mac2 = bdaddr.addr;
+			set_paired_mac(fd, mac2);
+			u8 mac[6] = {0};
+			get_bdaddrs(fd, mac, NULL);
+			printf("\nController's pair address set to: ");
+			print_mac(mac);
+			
+			memcpy(bd2.addr, mac2, sizeof(uint8_t) * 6);
+			if (bd_addr_cmp(&bdaddr, &bd2)) {
+				printf("\n\nAddress set correctly! Press HOME to exit.\n");	  
+			} else {
+				printf("\n\nPair MAC Address could not be set correctly.\n");	
+			}
+			
+			USB_CloseDevice(&fd);
+			return;
+		}
+	}
+	printf("No controller found on USB busses.\n");
+}
+
+int set_paired_mac(int fd, unsigned char *mac)
+{
+	u8 ATTRIBUTE_ALIGN(32) buf[] = {
+		0x13,
+		mac[5], mac[4], mac[3], mac[2], mac[1], mac[0],
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	};
+	return USB_WriteCtrlMsg(fd,
+		USB_REQTYPE_INTERFACE_SET,
+		USB_REQ_SETCONFIG,
+		(USB_REPTYPE_FEATURE<<8) | 0x13,
+		0,
+		sizeof(buf),
+		buf);
+}
+
+int get_link_key(int fd, unsigned char *link_key)
+{
+	u8 ATTRIBUTE_ALIGN(32) buf[0x10];
+	int ret = USB_ReadCtrlMsg(fd,
+		USB_REQTYPE_INTERFACE_GET,
+		USB_REQ_CLEARFEATURE,
+		(USB_REPTYPE_FEATURE<<8) | 0x13,
+		0,
+		sizeof(buf),
+		buf);
+		
+	int i;
+	for(i = 0; i < ret; i++) {
+		link_key[i] = buf[i];
+	}
+	return ret;
+}
+
+int get_bdaddrs(int fd, unsigned char *paired_mac, unsigned char *ds4_mac)
+{
+	u8 ATTRIBUTE_ALIGN(32) buf[0x10];
+	int ret = USB_ReadCtrlMsg(fd,
+		USB_REQTYPE_INTERFACE_GET,
+		USB_REQ_CLEARFEATURE,
+		(USB_REPTYPE_FEATURE<<8) | 0x12,
+		0,
+		sizeof(buf),
+		buf);
+	
+	if (paired_mac) {
+		paired_mac[0] = buf[15];
+		paired_mac[1] = buf[14];
+		paired_mac[2] = buf[13];
+		paired_mac[3] = buf[12];
+		paired_mac[4] = buf[11];
+		paired_mac[5] = buf[10];
+	}
+	if (ds4_mac) {
+		ds4_mac[0] = buf[6];
+		ds4_mac[1] = buf[5];
+		ds4_mac[2] = buf[4];
+		ds4_mac[3] = buf[3];
+		ds4_mac[4] = buf[2];
+		ds4_mac[5] = buf[1];
+	}
+
+	return ret;
+}
+
+
+int bte_read_bdaddr_cb(s32 result, void *userdata)
+{
+	bd_addr_read = 1;
+	return 1;
+}
+
+void print_mac(uint8_t *mac)
+{
+	printf("%02X:%02X:%02X:%02X:%02X:%02X",mac[0],mac[1],mac[2],mac[3],mac[4],mac[5]);
+}
+
+void init_video()
+{
+	VIDEO_Init();
+	rmode = VIDEO_GetPreferredMode(NULL);
+	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+	console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
+	VIDEO_Configure(rmode);
+	VIDEO_SetNextFramebuffer(xfb);
+	VIDEO_SetBlack(FALSE);
+	VIDEO_Flush();
+	VIDEO_WaitVSync();
+	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
+	printf("\x1b[1;0H");
+}
